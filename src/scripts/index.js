85cc32d2250ae67ce7fb66b3c8629096cc81a5b0
suxxus <suxxus@outlook.com>
2019-09-05 12:24:53 +0200

    Tests added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,67 @@
 const daggy = require('daggy');
+//const _ = lodash;
 
-const fn = () => true;
+const connectionTypes = {
+  GAS: 'GAS',
+  COMBI: 'COMBI',
+};
+
+const buildingTypes = {
+  APARTMENT_BUILDING: 'APARTMENT_BUILDING',
+  OFFICE_BUILDING: 'OFFICE_BUILDING',
+  DETACHED_HOUSE: 'DETACHED_HOUSE',
+};
+
+const isStepValid = ({
+  connectionType,
+  connectionTypesList,
+  buildingType,
+  buildingTypesList,
+}) => {
+  if (connectionType && buildingType) {
+    if (connectionTypesList && buildingTypesList) {
+      return (
+        connectionTypesList.includes(connectionType) &&
+        buildingTypesList.includes(buildingType)
+      );
+    }
+  }
+
+  if (connectionType) {
+    if (Array.isArray(connectionTypesList)) {
+      return connectionTypesList.includes(connectionType);
+    }
+  }
+
+  if (buildingType) {
+    if (Array.isArray(buildingTypesList)) {
+      return buildingTypesList.includes(buildingType);
+    }
+  }
+
+  return false;
+};
+const steps = daggy.taggedSum('steps', {
+  powerRequirementGas: [],
+});
+
+const getStep = (connectionType, buildingType) => {
+  const stepsPermissions = {
+    powerRequirementGas: {
+      buildingTypes: [
+        buildingTypes.APARTMENT_BUILDING,
+        buildingTypes.OFFICE_BUILDING,
+        buildingTypes.DETACHED_HOUSE,
+      ],
+      connectionTypes: [connectionTypes.COMBI, connectionTypes.GAS],
+    },
+  };
+};
 
 module.exports = {
-  fn,
+  connectionTypes,
+  buildingTypes,
+  isStepValid,
+  steps,
+  getStep,
 };
--- src/tests/scripts/index.js
+++ src/tests/scripts/index.js
@@ -1,11 +1,86 @@
 const test = require('tape');
-const {
-fn
-} = require('scripts');
+const { isStepValid, getStep, steps } = require('scripts');
 
-test('fn', t => {
-  const actual = fn();
+const getRandomItemFromList = myArray =>
+  myArray[Math.floor(Math.random() * myArray.length)];
+
+test('Given an object with a list for the connectionTypes and a list for the buildingTypes, return true if the list includes the value or the connectionType and the value of the buildingType', t => {
+  const ob = {
+    connectionType: getRandomItemFromList(['GAS', 'COMBI']),
+    connectionTypesList: ['GAS', 'COMBI'],
+    buildingType: getRandomItemFromList([
+      'APARTMENT_BUILDING',
+      'OFFICE_BUILDING',
+      'DETACHED_HOUSE',
+    ]),
+    buildingTypesList: [
+      'APARTMENT_BUILDING',
+      'OFFICE_BUILDING',
+      'DETACHED_HOUSE',
+    ],
+  };
+
+  const actual = isStepValid(ob);
   const expect = true;
+
   t.equal(actual, expect);
+  t.end();
+});
+
+test('Given an object with a list for the connectionTypes and a value of the connectionType, return true if the list includes the value of the connectionType', t => {
+  const ob = {
+    connectionType: getRandomItemFromList(['GAS', 'COMBI']),
+    connectionTypesList: ['GAS', 'COMBI'],
+  };
+
+  const actual = isStepValid(ob);
+  const expect = true;
+
+  t.equal(actual, expect);
+  t.end();
+});
+
+test('Given an object with a list for the buildingTypesList and a value of the buildingType, return true if the list includes the value of the buildingType', t => {
+  const ob = {
+    buildingType: getRandomItemFromList([
+      'APARTMENT_BUILDING',
+      'OFFICE_BUILDING',
+      'DETACHED_HOUSE',
+    ]),
+    buildingTypesList: [
+      'APARTMENT_BUILDING',
+      'OFFICE_BUILDING',
+      'DETACHED_HOUSE',
+    ],
+  };
+
+  const actual = isStepValid(ob);
+  const expect = true;
+
+  t.equal(actual, expect);
+  t.end();
+});
+
+test('Given an empty object shoudl return false', t => {
+  const actual = isStepValid({});
+  const expect = false;
+
+  t.equal(actual, expect);
+  t.end();
+});
+
+test('Given the connectionType and the buildingType, shoud return the step type', t => {
+  const connectionType = getRandomItemFromList(['GAS', 'COMBI']);
+  const buildingType = getRandomItemFromList([
+    'APARTMENT_BUILDING',
+    'OFFICE_BUILDING',
+    'DETACHED_HOUSE',
+  ]);
+
+  const actual = getStep(connectionType, buildingType);
+  const expect = steps.powerRequirementGas;
+
+  //t.equal(actual, expect);
+
   t.end();
 });
